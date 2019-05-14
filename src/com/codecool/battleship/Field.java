bdd58c1c81ba50919c69b2a91b175631b68126a8
Andras Molnar <andris9016@gmail.com>
2019-05-14 19:50:32 +0200

    Implement basic player input

--- src/com/codecool/battleship/Field.java
+++ src/com/codecool/battleship/Field.java
@@ -2,11 +2,16 @@ package com.codecool.battleship;
 
 public class Field {
 
-    public static void printField() {
+    public static void printField(int[][] arr) {
         for (int i = 0; i < 10; i++) {
             System.out.print("        ");
             for (int j = 0; j < 10; j++) {
-                System.out.print("~ ");
+                if(arr[i][j] == 0) {
+                    System.out.print("~ ");
+                } else if(arr[i][j] == 1) {
+                    System.out.print("@ ");
+                }
+
             }
             System.out.println();
         }
