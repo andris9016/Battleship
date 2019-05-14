06a5ba61092159391c93b6aa6c6750814c1a5031
Andras Molnar <andris9016@gmail.com>
2019-05-14 20:14:28 +0200

    Implement Player class with a few method

--- src/com/codecool/battleship/Game.java
+++ src/com/codecool/battleship/Game.java
@@ -1,6 +1,5 @@
 package com.codecool.battleship;
 
-import java.util.Scanner;
 
 public class Game {
 
@@ -16,26 +15,14 @@ public class Game {
         System.out.println("      Right now, the sea is empty\n");
         
         Field field1 = new Field();
+        Player player1 = new Player();
+
         field1.printField(fieldArray);
-        playerInput(fieldArray);
+        player1.playerInput(fieldArray);
         field1.printField(fieldArray);
+        player1.playerTurn(fieldArray);
     }
 
-    public static void playerInput(int[][] arr) {
-        int x, y;
-        Scanner reader = new Scanner(System.in);
-        System.out.print("Enter your x coordinate: ");
-        x = reader.nextInt();
-        System.out.print("Enter your y coordinate: ");
-        y = reader.nextInt();
-        if ((x >= 10) || (y >= 10)) {
-            System.out.println("invalid");
-        } else if (arr[x][y] == 1) {
-            System.out.println("already entered,plz enter again:");
-        } else {
-            arr[x][y] = 1;
-            System.out.println("YOUR SHIP is deployed x = " + x + " y = " + y);
-        }
-    }
+    
 
 }
---
+++ src/com/codecool/battleship/Player.java
@@ -0,0 +1,39 @@
+package com.codecool.battleship;
+
+import java.util.Scanner;
+
+
+public class Player {
+
+    public static void playerInput(int[][] arr) {
+        int x, y;
+        Scanner reader = new Scanner(System.in);
+        System.out.print("Enter your x coordinate: ");
+        x = reader.nextInt();
+        System.out.print("Enter your y coordinate: ");
+        y = reader.nextInt();
+        if ((x >= 10) || (y >= 10)) {
+            System.out.println("Invalid");
+        } else if (arr[x][y] == 1) {
+            System.out.println("Already entered this point");
+        } else {
+            arr[x][y] = 1;
+            System.out.println("YOUR SHIP is deployed x = " + x + " y = " + y);
+        }
+    }
+
+    public static void playerTurn(int[][] arr) {
+        int x, y;
+        Scanner reader = new Scanner(System.in);
+        System.out.println("ATTACK");
+        System.out.print("Pleas enter the x coordinate: ");
+        x = reader.nextInt();
+        System.out.print("Please enter the y coordinate: ");
+        y = reader.nextInt();
+        if ((x >= 10) || (y >= 10)) {
+            System.out.println("Invalid");
+        } else if (arr[x][y] == 1) {
+            System.out.println("You hit the enemy! And you WON!!!");
+        }
+    }
+}
