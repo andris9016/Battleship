c6bbd7421eb5667e46bca245cafcabec62e14458
lazoli95 <zoltan.lazar1995@gmail.com>
2019-05-16 16:26:30 +0200

    modified String content, clear the terminal

--- src/com/codecool/battleship/Game.java
+++ src/com/codecool/battleship/Game.java
@@ -5,21 +5,21 @@ import com.codecool.termlib.*;
 public class Game {
 
     public static void main(String[] args) {
-	Terminal terminal = new Terminal();
-		
-	Menu startmsg = new Menu();
-	startmsg.start();	
-	String firstPlayer = startmsg.getPlayer1Name();
-	String secondPlayer = startmsg.getPlayer2Name();
-	terminal.clearScreen();
+	//Menu startmsg = new Menu();
+	//String firstPlayer = startmsg.getPlayer1Name();
+	//String secondPlayer = startmsg.getPlayer2Name();	
+	//System.out.print(firstPlayer);
+	//System.out.print(secondPlayer);
 
         
-  
+        Terminal terminal = new Terminal();
         Color redColor = Color.RED;
+	Color greenColor = Color.CYAN;
         String red = terminal.getColor(redColor);
+	String green = terminal.getColor(greenColor);
         String reset = terminal.getResetStyle();
 
-        System.out.println("\n              " + red + "**** Welcome to Battle Ships game "+firstPlayer+ " and " +secondPlayer+ " ****");
+        System.out.println("\n              " + red + "**** Welcome to Battle Ships game ****");
         System.out.println("                    Right now, the sea is empty\n" + reset);
         
         Field field1 = new Field();
@@ -33,17 +33,18 @@ public class Game {
 
         
         field1.printField(fieldArray1);
-        System.out.println(firstPlayer);
+        System.out.println(green + "First player" + reset);
         player1.playerInput(fieldArray1);
-        field1.printField(fieldArray1);
-        
-        System.out.println(secondPlayer);
-        player2.playerInput(fieldArray2);
-        field2.printField(fieldArray2);
+	terminal.clearScreen();
 
-        System.out.println(firstPlayer);
+        
+        System.out.println(green + "Second player" + reset);
+        player2.playerInput(fieldArray2);
+	terminal.clearScreen();
+
+        System.out.println(green + "First player" + reset);
         player1.playerTurn(fieldArray2);
-        System.out.println(secondPlayer);
+        System.out.println(green + "Second player" + reset);
         player2.playerTurn(fieldArray1);
     }
 
--- src/com/codecool/battleship/Player.java
+++ src/com/codecool/battleship/Player.java
@@ -12,8 +12,45 @@ public class Player {
 	Terminal terminal = new Terminal();
 	Color magentaColor = Color.MAGENTA;
 	Color redColor = Color.RED;
+	Color greenColor = Color.GREEN;
+
+	// Get ANSI codes
+	String magenta = terminal.getColor(magentaColor);
+	String red = terminal.getColor(redColor);
+	String green = terminal.getColor(greenColor);
+        String reset = terminal.getResetStyle();
+
+
+        Scanner reader = new Scanner(System.in);
+        System.out.print("\nEnter your " + magenta + "Row" + reset + " coordinate to place your Ship: \n ");
+        x = reader.nextInt()-1;
+        System.out.print("\nEnter your " + magenta + "Column" + reset + " coordinate to place your Ship: \n");
+        y = reader.nextInt()-1;
+        while((x >= 10) || (y >= 10)) {
+            System.out.println(red + "\nInvalid coordinates!\n" + reset);
+            System.out.print("\nEnter your " + magenta + "Row" + reset + " coordinate to place your Ship: \n ");
+            x = reader.nextInt()-1;
+            System.out.print("\nEnter your " + magenta + "Column" + reset + " coordinate to place your Ship: \n ");
+            y = reader.nextInt()-1;
+        }
+        if (arr[x][y] == 1) {
+            System.out.println(red + "This field is not Empty" + reset);
+        } else {
+            arr[x][y] = 1;
+        }
+    }
+
+    public static void playerTurn(int[][] arr) {
+        int x, y;
+
+	// set colors 
+	Terminal terminal = new Terminal();
+	Color magentaColor = Color.MAGENTA;
+	Color redColor = Color.RED;
 	Color yellowColor = Color.YELLOW;
 	Color greenColor = Color.GREEN;
+
+	// Get ANSI codes
 	String magenta = terminal.getColor(magentaColor);
 	String red = terminal.getColor(redColor);
 	String yellow = terminal.getColor(yellowColor);
@@ -22,44 +59,7 @@ public class Player {
 
 
         Scanner reader = new Scanner(System.in);
-        System.out.print("\nEnter your " + magenta + "Row" + reset + " coordinate: \n ");
-        x = reader.nextInt()-1;
-        System.out.print("\nEnter your " + magenta + "Column" + reset + " coordinate: \n");
-        y = reader.nextInt()-1;
-        while((x >= 10) || (y >= 10)) {
-            System.out.println(red + "\nInvalid coordinates!\n" + reset);
-            System.out.print("\nEnter your " + magenta + "Row" + reset + " coordinate: \n ");
-            x = reader.nextInt()-1;
-            System.out.print("\nEnter your " + magenta + "Column" + reset + " coordinate: \n ");
-            y = reader.nextInt()-1;
-        }
-        if (arr[x][y] == 1) {
-            System.out.println(red + "This field is not Empty" + reset);
-        } else {
-            arr[x][y] = 1;
-            System.out.println(yellow + "\nYOUR SHIP is deployed x = " + (x+1) + " y = " + (y+1) + reset + "\n");
-        }
-    }
-
-    public static void playerTurn(int[][] arr) {
-        int x, y;
-
-
-        // set colors 
-        Terminal terminal = new Terminal();
-        Color magentaColor = Color.MAGENTA;
-        Color redColor = Color.RED;
-        Color yellowColor = Color.YELLOW;
-        Color greenColor = Color.GREEN;
-        String magenta = terminal.getColor(magentaColor);
-        String red = terminal.getColor(redColor);
-        String yellow = terminal.getColor(yellowColor);
-        String green = terminal.getColor(greenColor);
-        String reset = terminal.getResetStyle();
-
-
-        Scanner reader = new Scanner(System.in);
-        System.out.println(red + "ATTACK!!!" + reset);
+        System.out.println(red + "\nATTACK!!!" + reset);
         System.out.print("\nEnter your " + magenta + "Row" + reset + " coordinate: \n ");
         x = reader.nextInt()-1;
         System.out.print("\nEnter your " + magenta + "Column" + reset + " coordinate: \n");
@@ -67,7 +67,7 @@ public class Player {
         if ((x >= 10) || (y >= 10)) {
             System.out.println(red + "\nInvalid coordinates!\n" + reset);
         } else if (arr[x][y] == 1) {
-            System.out.println(green + "You hit the enemy! And you WON!!!" + reset);
+            System.out.println(green + "\nYou hit the enemy! And you WON!!!" + reset);
         }
     }
 }
