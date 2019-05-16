2a7dd7e5e6fce09cdf80d09070ba0e8ecfba7bd0
Sáfár Péter <safpeter03@gmail.com>
2019-05-16 16:37:39 +0200

    Can not give wrong input

--- src/com/codecool/battleship/Player.java
+++ src/com/codecool/battleship/Player.java
@@ -1,39 +1,52 @@
 package com.codecool.battleship;
 
-import java.util.Scanner;
+import java.util.*;
 
 
 public class Player {
 
     public static void playerInput(int[][] arr) {
-        int x, y;
+        int rowCoordinate , colCoordinate;
         Scanner reader = new Scanner(System.in);
-        System.out.print("\nEnter your x coordinate: ");
-        x = reader.nextInt();
-        System.out.print("\nEnter your y coordinate: ");
-        y = reader.nextInt();
-        if ((x >= 10) || (y >= 10)) {
-            System.out.println("Invalid");
-        } else if (arr[x][y] == 1) {
-            System.out.println("Already entered this point");
-        } else {
-            arr[x][y] = 1;
-            System.out.println("\nYOUR SHIP is deployed x = " + x + " y = " + y);
+        do {  System.out.println("\nEnter your x coordinate between 1 & 10: ");
+            while (!reader.hasNextInt()){
+            System.out.println("Give me a number between 1 & 10!!");
+            reader.next();
+            }
+            rowCoordinate = reader.nextInt();
+        }while ( rowCoordinate >10 || rowCoordinate < 1);
+        do { System.out.print("\nEnter your y coordinate between 1 and 10: ");
+            while (!reader.hasNextInt()){
+            System.out.println("Give me a number between 1 & 10");
+            reader.next();
+            }
+            colCoordinate = reader.nextInt();
+        }while (colCoordinate>10 || colCoordinate<1);
+            System.out.println("\nYOUR SHIP is deployed row = " + rowCoordinate + "col  = " + colCoordinate);
         }
-    }
-
-    public static void playerTurn(int[][] arr) {
-        int x, y;
+     public static void playerTurn(int[][] arr) {
+        int rowCoordinate, colCoordinate;
         Scanner reader = new Scanner(System.in);
-        System.out.println("ATTACK");
-        System.out.print("Pleas enter the x coordinate: ");
-        x = reader.nextInt();
-        System.out.print("Please enter the y coordinate: ");
-        y = reader.nextInt();
-        if ((x >= 10) || (y >= 10)) {
-            System.out.println("Invalid");
-        } else if (arr[x][y] == 1) {
+        do { System.out.println("ATTACK");
+             System.out.println("\nEnter your y coordinate between 1 and 10: ");
+            while (!reader.hasNextInt() ){
+            System.out.println("Give me a number between 1 & 10");
+            reader.next();
+            }
+            rowCoordinate = reader.nextInt();
+        }while (rowCoordinate > 10 || rowCoordinate < 1);
+        do { System.out.println("ATTACK");
+             System.out.println("\nEnter your y coordinate between 1 and 10: ");
+            while (!reader.hasNextInt() ){
+            System.out.println("Give me a number between 1 & 10");
+            reader.next();
+            }
+            colCoordinate = reader.nextInt();
+        }while (colCoordinate>10 || colCoordinate<1);
+            colCoordinate = reader.nextInt();
+        if (arr[rowCoordinate][colCoordinate] == 1) {
             System.out.println("You hit the enemy! And you WON!!!");
         }
     }
+  
 }
